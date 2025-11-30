44775d2cb7c52c51cfae32328c022ac37d4a5b11
David Roman <officialdavidromanyt@gmail.com>
2025-11-30 13:23:21 -0400

    level systejm

--- os/apps/game_center/games/mario/mario.c
+++ os/apps/game_center/games/mario/mario.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include "mario.h"
 
+
+//structs 
 struct Vector2{
     int x, y;
 };
@@ -15,25 +17,34 @@ struct Camera{
 
 struct Player{
     struct Vector2 position;
+    struct Vector2 velocity;
     int is_on_ground;
 
 };
 
 
-static int fps = 30;
+
+
+
+//globals
+struct Player player;
 static int is_in_menu;
-
-static int screen_w = 600;
-static int screen_h = 600;
-
 int menu_options = 2;
 int pointer = 1; 
 static int end_game;
+static int current_level;
 
-static int current_world;
+
+
+
+//settings
+static int fps = 30;
+static int screen_w = 600;
+static int screen_h = 600;
+static int gravity = 2;
+static int mario_offset = 50; //height offset so hes no clipping into tiles
 
 //colors
-
 static uint32_t red = 0x00000FFFF;
 static uint32_t dark_brown = 0x8000FF8F;
 static uint32_t light_brown = 0x3FFFFF3F;
@@ -45,7 +56,33 @@ static uint32_t light_brown = 0x3FFFFF3F;
 Sprite mario_sprite;
 
 
+//levels
+#define TILE_SIZE 40
+    
+//rules:
+    //0 air, 1 brick, 2 question block
 
+    //level 1
+    #define l1_rows 15
+    #define l1_cols 20
+
+    int l1_map[l1_rows][l1_cols] = {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
 
 
 
@@ -72,12 +109,13 @@ static int menu(){
     if (pointer<1){pointer = 1;} 
     if (pointer>menu_options){pointer = menu_options;} 
 
-    if (is_pressed(' ')&& pointer ==3){
+
+    if (is_pressed(' ')&& pointer ==2){
         end_game = 1;
     }
 
-    if (is_pressed(' ')){
-        return pointer;
+    if (is_pressed(' ')&& pointer == 1){
+        is_in_menu =0;
     }
 
 
@@ -108,16 +146,98 @@ static int menu(){
 }
 
 
+void render_level(int* level, int level_cols, int level_rows) {
+    for (int row = 0; row < level_rows; row++) {
+        for (int col = 0; col < level_cols; col++) {
+            
+          
+            int current_index = (row * level_cols) + col;
+
+        
+            int tile_id = level[current_index];
+
+            
+            if (tile_id > 0) {
+                int x_pos = col * TILE_SIZE;
+                int y_pos = row * TILE_SIZE;
+                
+                uint32_t color = (tile_id == 1) ? dark_brown : light_brown;
+               
+                draw_rect(x_pos, y_pos, TILE_SIZE, TILE_SIZE, color); 
+            }
+        }
+    }
+}
+
+void update_physics(int* level, int level_cols, int level_rows) {
+   
+    player.velocity.y += gravity; 
+
+    //apply velocity to pos
+    player.position.y += player.velocity.y;
+    player.position.x += player.velocity.x; 
+
+    //floor check
+    int map_col = (player.position.x + 15) / TILE_SIZE; //mario center
+    int map_row = (player.position.y + mario_offset) / TILE_SIZE;
+
+    //check bounds
+    if (map_row >= 0 && map_row < level_rows && map_col >= 0 && map_col < level_cols) {
+
+        int tile_index = (map_row * level_cols) + map_col;
+        
+        //if air
+        if (level[tile_index] != 0) {
+            
+            //snap mario
+            player.position.y = (map_row * TILE_SIZE) - mario_offset; 
+            player.velocity.y = 0;
+            player.is_on_ground = 1;
+        } else {
+            player.is_on_ground = 0;
+        }
+    }
+}
+
+
+
+static void input_poll(){
+    if (is_in_menu==1){return;}
+    
+    player.velocity.x = 0; 
+    int speed = 8;
+
+    if (is_pressed('d')){
+        player.velocity.x = speed;
+    }
+
+    if (is_pressed('a')){
+        player.velocity.x = -speed;
+    }
+
+
+    if (is_pressed(' ') && player.is_on_ground){
+        player.velocity.y = -20; 
+        player.is_on_ground = 0;
+    }
+}
 
 static void main_loop() { 
     while (end_game == 0) {
         if (should_update()) {
             screen_clear();
-        
+            input_poll();
             if (is_in_menu==1) menu();
 
-            draw_sprite(25, 25, mario_sprite, 10);
+            else{
 
+            input_poll();
+            update_physics(l1_map, l1_cols, l1_rows); 
+            render_level(l1_map, l1_cols, l1_rows);  
+            draw_sprite(player.position.x, player.position.y, mario_sprite, 3);
+            
+        
+            }
 
             
             flip(); 
